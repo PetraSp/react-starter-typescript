34a559d93a9bbf3f5e405a31e643a4f3c97aa701
Petra Spirkova <pspirkova@gmail.com>
2019-06-06 11:09:51 +0200

    change default import to import React

--- src/containers/DataTable/index.tsx
+++ src/containers/DataTable/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
